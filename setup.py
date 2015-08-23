7945f314ff172c041766227e0e5c7d182a12a802
Jonathan Eunice <jonathan.eunice@gmail.com>
2015-08-23 18:49:56 -0400

    1.2.7

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def lines(text):
 
 setup(
     name='simplere',
-    version='1.2.6',
+    version='1.2.7',
     author='Jonathan Eunice',
     author_email='jonathan.eunice@gmail.com',
     description='Simpler, cleaner access to regular expressions. Globs too.',
